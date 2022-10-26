a54629238c2668be37b06cf17568efd40a910592
valentinesamuel <valentinesamuel2580@gmail.com>
2022-10-26 21:55:41 +0100

    prettied

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, {Application, NextFunction, Request, Response} from 'express';
 import cors from 'cors';
 
 const app: Application = express();
 const PORT: Number = 5000;
 
-app.use(cors({
-	origin: '*'
-}))
+app.use(
+	cors({
+		origin: '*'
+	})
+);
 app.use('/', (_req: Request, res: Response, _next: NextFunction) => {
 	res.status(200).send({data: 'Hello from my own personal server'});
 });
